7b282b1b51c2ae0863317cebd0110dbdec4320c4
mohanapriyamphil <priyamphilmaths@gmail.com>
2024-05-18 23:01:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function addData() {
+    var firstName = document.getElementById('first-name').value;
+    var lastName = document.getElementById('last-name').value;
+    var email = document.getElementById('email').value;
+    var address = document.getElementById('address').value;
+    var pincode = document.getElementById('pincode').value;
+    var state = document.getElementById('state').value;
+    var country = document.getElementById('country').value;
+
+    var table = `<tr>
+                    <td>${firstName}</td>
+                    <td>${lastName}</td>
+                    <td>${email}</td>
+                    <td>${address}</td>
+                    <td>${pincode}</td>
+                    <td>${state}</td>
+                    <td>${country}</td>`;
+
+    document.getElementById('tableData').innerHTML += table;
+    clearForm();
+}
+
+function clearForm() {
+    document.getElementById('lastName').value = "";
+    document.getElementById('email').value = "";
+    document.getElementById('address').value = "";
+    document.getElementById('pincode').value = "";
+    document.getElementById('state').value = "";
+    document.getElementById('country').value = "";
+}
